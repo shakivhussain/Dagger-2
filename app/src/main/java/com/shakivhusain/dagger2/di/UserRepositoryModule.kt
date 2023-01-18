9d2dad0c8d85a7cdb0e80c89b3d476fe926a39d6
shakibmansoori <sakib@joinzorro.com>
2023-01-18 23:52:33 +0530

    @Module, @Provides, @Binds Annotation

--- app/src/main/java/com/shakivhusain/dagger2/di/UserRepositoryModule.kt
+++ app/src/main/java/com/shakivhusain/dagger2/di/UserRepositoryModule.kt
@@ -1,19 +1,30 @@
 package com.shakivhusain.dagger2.di
 
 import com.shakivhusain.dagger2.FirebaseRepository
+import com.shakivhusain.dagger2.SQLRepository
 import com.shakivhusain.dagger2.UserRepository
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 
 @Module
-class UserRepositoryModule {
+abstract class UserRepositoryModule {
 
     // Whenever you need UsrRepo, this funtion will provide Firebase Obj
     //
-    @Provides
-    fun getFirebaseRepository() : UserRepository{
-        return FirebaseRepository()
-    }
+//    @Provides
+//    fun getFirebaseRepository(sqlRepository: SQLRepository) : UserRepository{
+//        return sqlRepository //
+//        // Dagger can create the object of sqlRepo cause, its inject with already
+//    }
 
+
+    // Or Can Use Bind
+
+    @Binds
+    abstract fun getFirebaseRepository(sqlRepository: SQLRepository) : UserRepository
+    // Here we are using bind, Bind Usr repo with sql repo
+    // SQL Repo Object can create by itself dagger, so we use bind, otherwise Provide
+    // If dagger can create obj by self then we can use bind =, otherwise provide
 
 }
